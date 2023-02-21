8aff7902d26187ddfba72dff7c92a3705ab88ea1
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-02-21 15:09:33 -0500

    Delete .idea directory

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import config from "./config.js";
+
+// 1. get all the html elements DOM
+// 2. listen to the even on the prompt input
+// 2a. on the blur event, call the image generate API
+// 2b. show the image in the browser
+
+console.log("this works");
