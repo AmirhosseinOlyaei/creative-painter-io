84170aef48680c83be98c16c07a5f14b7937940f
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-02-24 14:20:43 -0500

    closes #7, API calls

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 import config from "./config.js";
 
-// 1. get all the html elements DOM
 // 2. listen to the even on the prompt input
 // 2a. on the blur event, call the image generate API
 // 2b. show the image in the browser
 
-console.log("this works");
+// 1. get all the html elements DOM
+const searchBoxEl = document.querySelector('#search-box');
+const imageResults = document.querySelector('#images');
+
+// 2. listen to the event on the prompt input
+searchBoxEl.addEventListener('blur', async () => {
+    // 2a. on the blur event, call the image generate API
+    const response = await fetch('https://api.openai.com/v1/images/generations', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${config.API_KEY}`,
+        },
+        body: JSON.stringify({
+            model: 'image-alpha-001',
+            prompt: `civil rights movement ${searchBoxEl.value}`,
+            num_images: 1,
+            size: '256x256',
+            response_format: 'url',
+        }),
+    });
+
+    // 2b. show the image in the browser
+    const data = await response.json();
+    const imgUrl = data.data[0].url;
+    const imgEl = document.createElement('img');
+    imgEl.src = imgUrl;
+    imageResults.appendChild(imgEl);
+});
