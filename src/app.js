d47fde132f2ea059a841a8dc85f1b00ddb4642af
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-02-24 14:50:18 -0500

    closes #11, API request

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import config from "./config.js";
 
-// 2. listen to the even on the prompt input
-// 2a. on the blur event, call the image generate API
-// 2b. show the image in the browser
-
 // 1. get all the html elements DOM
 const searchBoxEl = document.querySelector('#search-box');
 const imageResults = document.querySelector('#images');
+
+console.log(searchBoxEl); // Add this line
 
 // 2. listen to the event on the prompt input
 searchBoxEl.addEventListener('blur', async () => {
