37b3cff67cfef3658383aee7ef8c32380fc30d25
Amirhossein Olyaei <20806019+AmirhosseinOlyaei@users.noreply.github.com>
2023-02-26 00:02:26 -0500

    solves #15 - Add a loading GIF

--- src/app.js
+++ src/app.js
@@ -4,13 +4,21 @@ import config from "../config.js";
 const searchBoxEl = document.querySelector('#search-box');
 const imageResults = document.querySelector('#images');
 
-console.log(searchBoxEl); // Add this line
-
 // 2. listen to the event on the prompt input
 searchBoxEl.addEventListener('blur', async () => {
     // Clear the existing images before making the API call
     imageResults.innerHTML = '';
-    // 2a. on the blur event, call the image generate API
+
+    // 2a. Add a loading GIF
+    const loadingGifEl = document.createElement('img');
+    loadingGifEl.src = 'src/image_processing.gif';
+    loadingGifEl.style.width = '200px'; // set the width of the loading GIF
+    loadingGifEl.style.height = '150px'; // set the height of the loading GIF
+    loadingGifEl.style.borderRadius = '25%'; // set the border-radius of the loading GIF
+    imageResults.appendChild(loadingGifEl);
+
+
+    // 2b. on the blur event, call the image generate API
     const response = await fetch('https://api.openai.com/v1/images/generations', {
         method: 'POST',
         headers: {
@@ -26,11 +34,12 @@ searchBoxEl.addEventListener('blur', async () => {
         }),
     });
 
-    // 2b. show the image in the browser
+    // 2c. show the image in the browser
     const data = await response.json();
     const imgUrl = data.data[0].url;
     const imgEl = document.createElement('img');
     imgEl.src = imgUrl;
+    imageResults.innerHTML = '';
     imageResults.appendChild(imgEl);
 
 });
